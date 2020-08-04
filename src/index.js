70fa09f32b602d750536d42185be55d3517e216d
Ankit <ankitt@valuefy.com>
2020-08-04 16:16:50 +0530

    test: test

--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@
 import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 const AwesomeModule = ({}) => {
-    return (React.createElement(View, { style: styles.container },
-        React.createElement(Text, null, "AwesomeModule")));
+  return React.createElement(
+    View,
+    { style: styles.container },
+    React.createElement(Text, null, 'AwesomeModule')
+  );
 };
 export default AwesomeModule;
 const styles = StyleSheet.create({
-    container: {},
+  container: {},
 });
